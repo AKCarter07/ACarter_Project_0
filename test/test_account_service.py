2c55cb3559c69b41208673479a5b136a2320bede
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-07-06 18:46:18 -0700

    first presentation save point. No ability to do filter searches

--- test/test_account_service.py
+++ test/test_account_service.py
@@ -1,15 +1,363 @@
+import service.customer_service
 from service.account_service import AccountService
-from pytest_mock import mocker
+from models.user import User
+from models.account import Account
+from dao.user_dao import UserDao
+import pytest
+from exception.invalid_parameter import InvalidParamError
 
-acs = AccountService()
 
-def test_get_accounts():
+def test_get_accounts(mocker):
     # Arrange
-    def mock_get_accounts(self):
+    def mock_get_accounts(self, user_id):
+        if user_id == 1001:
+            return {'10010001': {'account number': '10010001', 'dollars': 10, 'cents': 10},
+                    '10010002': {'account number': '10010002', 'dollars': 10, 'cents': 10}}
+        else:
+            return None
+
+    def mock_dao_get_user(self, user_id):
+        if user_id == 1001:
+            user1 = User('user1', 1001)
+            user1.add_account(10, 10)
+            user1.add_account(10, 10)
+            return user1
+        else:
+            return None
+
+    def mock_acct_dao_get_account(self, account_id, user_id):
+        return Account(account_id, 10, 10)
+
+    mocker.patch('dao.account_dao.AccountDao.get_user_accounts', mock_get_accounts)
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_dao_get_user)
+    mocker.patch('dao.account_dao.AccountDao.get_account', mock_acct_dao_get_account)
+    acs = AccountService()
+    # Act
+    actual = acs.get_accounts(1001)
+
+    # Assert
+    assert actual == {'10010001': {'account number': '10010001', 'cents': 10, 'dollars': 10},
+                      '10010002': {'account number': '10010002', 'cents': 10, 'dollars': 10}}
+
+def test_get_account_positive(mocker):
+    def mock_cs_user_list(self):
+        return ['1001', '1002']
+
+    def mock_get_accounts(self, user_id):
+        if user_id == 1001:
+            return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                    '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+        else:
+            return None
+
+    def mock_get_account(self, user_id, account_id):
+        return Account(10010001, 25, 25)
+
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    def mock_get_accounts_via_user(self):
         return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
                 '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
 
-    mocker.mock('acs.AccountDao.get_accounts', mock_get_accounts())
-    # Act
+    mocker.patch('service.customer_service.CustomerService.user_id_list', mock_cs_user_list)
+    mocker.patch('dao.account_dao.AccountDao.get_user_accounts', mock_get_accounts)
+    mocker.patch('dao.account_dao.AccountDao.get_account', mock_get_account)
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+    mocker.patch('models.user.User.get_accounts', mock_get_accounts_via_user)
+    acs = AccountService()
 
-    # Assert
+    actual = acs.get_account(1001, 10010001)
+
+    assert actual == {'account number': '10010001', 'cents': 25, 'dollars': 25}
+
+def test_get_account_bad_user_id(mocker):
+    def mock_cs_user_list(self):
+        return ['1001', '1002']
+
+    def mock_get_accounts(self, user_id):
+        if user_id == 1001:
+            return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                    '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+        else:
+            return None
+
+    def mock_get_account(self, user_id, account_id):
+        return Account(10010001, 25, 25)
+
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    def mock_get_accounts_via_user(self):
+        return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+
+    mocker.patch('service.customer_service.CustomerService.user_id_list', mock_cs_user_list)
+    mocker.patch('dao.account_dao.AccountDao.get_user_accounts', mock_get_accounts)
+    mocker.patch('dao.account_dao.AccountDao.get_account', mock_get_account)
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+    mocker.patch('models.user.User.get_accounts', mock_get_accounts_via_user)
+    acs = AccountService()
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = acs.get_account(1003, 10010001)
+
+    assert str(excinfo.value) == 'User Id does not exist.'
+
+def test_get_account_neg_acct_not_found(mocker):
+    def mock_cs_user_list(self):
+        return ['1001', '1002']
+
+    def mock_get_accounts(self, user_id):
+        if user_id == 1001:
+            return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                    '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+        else:
+            return None
+
+    def mock_get_account(self, user_id, account_id):
+        return Account(10010001, 25, 25)
+
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    def mock_get_accounts_via_user(self):
+        return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+
+    mocker.patch('service.customer_service.CustomerService.user_id_list', mock_cs_user_list)
+    mocker.patch('dao.account_dao.AccountDao.get_user_accounts', mock_get_accounts)
+    mocker.patch('dao.account_dao.AccountDao.get_account', mock_get_account)
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+    mocker.patch('models.user.User.get_accounts', mock_get_accounts_via_user)
+    acs = AccountService()
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = acs.get_account(1001, 10010003)
+
+    assert str(excinfo.value) == 'Account 10010003 not found.'
+
+
+def test_withdraw_positive(mocker):
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+
+    def mock_edit_account(self, account_id, user_id, operation, dollars, cents):
+        if account_id == 10010001:
+            dollars = 25 - dollars
+            cents = 25 - cents
+            account = Account(10010001, dollars, cents)
+            return account
+        else:
+            return None
+
+    mocker.patch('dao.account_dao.AccountDao.edit_account', mock_edit_account)
+
+    def mock_get_accounts_via_user(self):
+        return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+
+    mocker.patch('models.user.User.get_accounts', mock_get_accounts_via_user)
+
+    acs = AccountService()
+    actual = acs.withdraw(1001, 10010001, 10, 10)
+
+    assert actual == "New balance: Account 10010001: Balance $15.15"
+
+def test_withdraw_neg_id_not_found(mocker):
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+
+    def mock_edit_account(self, account_id, user_id, operation, dollars, cents):
+        if account_id == 10010001:
+            dollars = 25 - dollars
+            cents = 25 - cents
+            account = Account(10010001, dollars, cents)
+            return account
+        else:
+            return None
+
+    mocker.patch('dao.account_dao.AccountDao.edit_account', mock_edit_account)
+
+    def mock_get_accounts_via_user(self):
+        return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+
+    mocker.patch('models.user.User.get_accounts', mock_get_accounts_via_user)
+
+    acs = AccountService()
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = acs.withdraw(1001, 10010003, 10, 10)
+    assert str(excinfo.value) == "Account 10010003 not found."
+
+def test_withdraw_neg_insufficient_funds(mocker):
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+
+    def mock_edit_account(self, account_id, user_id, operation, dollars, cents):
+        if account_id == 10010001:
+            dollars = 25 - dollars
+            cents = 25 - cents
+            account = Account(10010001, dollars, cents)
+            return account
+        else:
+            return None
+
+    mocker.patch('dao.account_dao.AccountDao.edit_account', mock_edit_account)
+
+    def mock_get_accounts_via_user(self):
+        return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+
+    mocker.patch('models.user.User.get_accounts', mock_get_accounts_via_user)
+
+    acs = AccountService()
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = acs.withdraw(1001, 10010001, 100, 10)
+    assert str(excinfo.value) == "You do not have sufficient funds."
+
+def test_deposit_positive(mocker):
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+
+    def mock_edit_account(self, account_id, user_id, operation, dollars, cents):
+        if account_id == 10010001:
+            dollars = 25 + dollars
+            cents = 25 + cents
+            account = Account(10010001, dollars, cents)
+            return account
+        else:
+            return None
+
+    mocker.patch('dao.account_dao.AccountDao.edit_account', mock_edit_account)
+
+    def mock_get_accounts_via_user(self):
+        return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+
+    mocker.patch('models.user.User.get_accounts', mock_get_accounts_via_user)
+
+    acs = AccountService()
+    actual = acs.deposit(1001, 10010001, 10, 10)
+
+    assert actual == "New balance: Account 10010001: Balance $35.35"
+
+def test_deposit_neg_id_not_found(mocker):
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+
+    def mock_edit_account(self, account_id, user_id, operation, dollars, cents):
+        if account_id == 10010001:
+            dollars = 25 + dollars
+            cents = 25 + cents
+            account = Account(10010001, dollars, cents)
+            return account
+        else:
+            return None
+
+    mocker.patch('dao.account_dao.AccountDao.edit_account', mock_edit_account)
+
+    def mock_get_accounts_via_user(self):
+        return {'10010001': {'account number': '10010001', 'dollars': 25, 'cents': 25},
+                '10010002': {'account number': '10010002', 'dollars': 100, 'cents': 50}}
+
+    mocker.patch('models.user.User.get_accounts', mock_get_accounts_via_user)
+
+    acs = AccountService()
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = acs.withdraw(1001, 10010003, 10, 10)
+    assert str(excinfo.value) == "Account 10010003 not found."
+
+def test_add_account(mocker):
+
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+
+    def mock_create_account(self, user_id, account_id, dollars, cents):
+        return f"Account {account_id} created with a starting balance of ${dollars}." \
+               f"{'0' if cents < 10 else ''}{cents}"
+
+    mocker.patch('dao.account_dao.AccountDao.create_account', mock_create_account)
+
+    def mock_dao_edit_user(self, user_id, new_info_object):
+        user = User('bobbert', 1001)
+        user.set_num_active_accounts(2)
+        return f"User has been updated: {user}"
+
+    mocker.patch('dao.user_dao.UserDao.edit_user', mock_dao_edit_user)
+
+    acs = AccountService()
+
+    actual = acs.add_account(1001, 10, 10)
+
+    assert actual == 'Account 10010003 created with a starting balance of $10.10'
+
+def test_delete_account_positive(mocker):
+    def mock_get_user_accounts(self, user_id):
+        user1 = User('user1', 1001)
+        user1.add_account(25, 25)
+        user1.add_account(100, 50)
+        return user1
+
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_get_user_accounts)
+
+    def mock_cs_user_id_list(self):
+        return ['1001', '1002']
+
+    mocker.patch('service.customer_service.CustomerService.user_id_list', mock_cs_user_id_list)
+
+    def mock_delete_account(self, user_id, account_id):
+        return f"Account {account_id} has been deleted."
+
+    def mock_dao_edit_user(self, user_id, new_info_object):
+        return f"User has been updated: {new_info_object}"
+
+    mocker.patch('dao.user_dao.UserDao.edit_user', mock_dao_edit_user)
+
+    acs = AccountService()
+
+    actual = acs.delete_account(1001, 10010001)
+
+    assert actual == 'Account 10010001 has been deleted.'
--- test/test_customer_service.py
+++ test/test_customer_service.py
@@ -1,43 +1,285 @@
 from models.user import User
 from service.customer_service import CustomerService
 import pytest
+from exception.invalid_parameter import InvalidParamError
 
 
-# def test_get_all_users(mocker):
-#     print("\nin mocker")
-#
-#     def mock_dao_get_all_users():
-#         user1 = User('user1', 1001)
-#         user1.set_num_active_accounts(2)
-#         user2 = User('user2', 1002)
-#         user2.set_num_active_accounts(1)
-#         users = [user1, user2]
-#         return users
-#
-#     mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users())
-#     cs = CustomerService()
-#
-#     actual = cs.get_all_users()
-#     print("printing actual")
-#     print(actual)
-#     assert actual == [1, 2]
+def test_get_all_users(mocker):
+    def mock_dao_get_all_users(self):
+        user1 = User('user1', 1001)
+        user1.set_num_active_accounts(2)
+        user2 = User('user2', 1002)
+        user2.set_num_active_accounts(1)
+        users = [user1, user2]
+        return users
+
+    mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users)
+    cs = CustomerService()
+
+    actual = cs.get_all_users()
+
+    assert actual == [
+        {
+            'username': 'user1',
+            'user_id': 1001,
+            'num_active_accounts': 2,
+            'active': True
+        },
+        {
+            'username': 'user2',
+            'user_id': 1002,
+            'num_active_accounts': 1,
+            'active': True
+        }
+    ]
 
 
-    # assert actual == [
-    #     {
-    #         'username': 'user1',
-    #         'user_id': 1001,
-    #         'num_active_accounts': 2,
-    #         'active': True
-    #     },
-    #     {
-    #         'username': 'user2',
-    #         'user_id': 1002,
-    #         'num_active_accounts': 1,
-    #         'active': True
-    #     }
-    # ]
+def test_get_user_positive(mocker):
+    def mock_dao_get_all_users(self):
+        user1 = User('user1', '1001')
+        user1.set_num_active_accounts(2)
+        user2 = User('user2', '1002')
+        user2.set_num_active_accounts(1)
+        users = [user1, user2]
+        return users
+
+    def mock_dao_get_user(self, user_id):
+        if user_id == 1001:
+            user1 = User('user1', 1001)
+            user1.set_num_active_accounts(2)
+            return user1
+        else:
+            return None
+
+    mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users)
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_dao_get_user)
+    cs = CustomerService()
+
+    actual = cs.get_user(1001)
+
+    assert actual == {
+        'username': 'user1',
+        'user_id': 1001,
+        'num_active_accounts': 2,
+        'active': True
+    }
+
+
+def test_get_user_negative(mocker):
+    def mock_dao_get_all_users(self):
+        user1 = User('user1', '1001')
+        user1.set_num_active_accounts(2)
+        user2 = User('user2', '1002')
+        user2.set_num_active_accounts(1)
+        users = [user1, user2]
+        return users
+
+    def mock_dao_get_user(self, user_id):
+        if user_id == 1001:
+            user1 = User('user1', 1001)
+            user1.set_num_active_accounts(2)
+            return user1
+        else:
+            return None
+
+    mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users)
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_dao_get_user)
+    cs = CustomerService()
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = cs.get_user(1003)
+
+    assert str(excinfo.value) == "User Id 1003 not found."
 
 
 def test_add_user_positive(mocker):
-    def mock_add_user(username):
+    def mock_dao_get_all_users(self):
+        user1 = User('user1', '1001')
+        user1.set_num_active_accounts(2)
+        user2 = User('user2', '1002')
+        user2.set_num_active_accounts(1)
+        users = [user1, user2]
+        return users
+
+    def mock_dao_add_user(self, user_object):
+        if user_object.get_idn() == 1003:
+            return "User user3 (1003) has been added to the system"
+        else:
+            return None
+
+    def mock_dao_number_idns(self):
+        return 2
+
+    mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users)
+    mocker.patch('dao.user_dao.UserDao.add_user', mock_dao_add_user)
+    mocker.patch('dao.user_dao.UserDao.number_idns', mock_dao_number_idns)
+    cs = CustomerService()
+
+    actual = cs.add_user("user3")
+
+    assert actual == 'User user3 (1003) has been added to the system'
+
+
+def test_add_user_negative_spaces_short(mocker):
+    def mock_dao_get_all_users(self):
+        user1 = User('user1', '1001')
+        user1.set_num_active_accounts(2)
+        user2 = User('user2', '1002')
+        user2.set_num_active_accounts(1)
+        users = [user1, user2]
+        return users
+
+    def mock_dao_add_user(self, user_object):
+        print(user_object)
+        if user_object.get_idn() == 1003:
+            return f"User {user_object.get_username()} (1003) has been added to the system"
+        else:
+            return None
+
+    def mock_dao_number_idns(self):
+        return 2
+
+    mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users)
+    mocker.patch('dao.user_dao.UserDao.add_user', mock_dao_add_user)
+    mocker.patch('dao.user_dao.UserDao.number_idns', mock_dao_number_idns)
+    cs = CustomerService()
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = cs.add_user(" sup")
+
+    assert str(excinfo.value) == 'Username cannot contain spaces.\nUsername must be' \
+                                 ' 5 or more characters.\n'
+
+def test_add_user_negative_usn_taken(mocker):
+    def mock_dao_get_all_users(self):
+        user1 = User('user1', '1001')
+        user1.set_num_active_accounts(2)
+        user2 = User('user2', '1002')
+        user2.set_num_active_accounts(1)
+        users = [user1, user2]
+        return users
+
+    def mock_dao_add_user(self, user_object):
+        print(user_object)
+        if user_object.get_idn() == 1003:
+            return f"User {user_object.get_username()} (1003) has been added to the system"
+        else:
+            return None
+
+    def mock_dao_number_idns(self):
+        return 2
+
+    mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users)
+    mocker.patch('dao.user_dao.UserDao.add_user', mock_dao_add_user)
+    mocker.patch('dao.user_dao.UserDao.number_idns', mock_dao_number_idns)
+    cs = CustomerService()
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = cs.add_user("user1")
+
+    assert str(excinfo.value) == 'Username user1 already exists.\n'
+
+
+def test_edit_user_positive(mocker):
+
+    def mock_dao_edit_user(self, user_id, new_info_object):
+        user = User('bobbert', 1001)
+        user.set_num_active_accounts(2)
+        return f"User has been updated: {user}"
+
+    mocker.patch('dao.user_dao.UserDao.edit_user', mock_dao_edit_user)
+    cs = CustomerService()
+
+    user1 = User('bobbert', 1001)
+    user1.set_num_active_accounts(2)
+
+    actual = cs.edit_user(1001, user1)
+
+    assert actual == 'User has been updated: \nbobbert (1001) has 2 active accounts: \n'
+
+
+def test_dao_edit_user_negative(mocker):
+    def mock_dao_edit_user(self, user_id, new_info_object):
+        user = User('bobbert', 1002)
+        user.set_num_active_accounts(2)
+        return f"User has been updated: {user}"
+
+    mocker.patch('dao.user_dao.UserDao.edit_user', mock_dao_edit_user)
+    cs = CustomerService()
+
+    user1 = User('bobbert', 2)
+    user1.set_num_active_accounts(2)
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = cs.edit_user(1001, user1)
+
+    assert str(excinfo.value) == 'Cannot change user Id.'
+
+def test_dao_delete_user_positive(mocker):
+
+    def mock_dao_get_all_users(self):
+        user1 = User('user1', '1001')
+        user1.set_num_active_accounts(2)
+        user2 = User('user2', '1002')
+        user2.set_num_active_accounts(1)
+        users = [user1, user2]
+        return users
+
+    def mock_dao_get_user(self, user_id):
+        if user_id == 1001:
+            user1 = User('user1', 1001)
+            user1.set_num_active_accounts(2)
+            return user1
+        else:
+            return None
+
+    def mock_dao_delete_user(self, user_object):
+        f"User account for {user_object.get_username()} has been deleted."
+
+    mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users)
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_dao_get_user)
+    mocker.patch('dao.user_dao.UserDao.delete_user', mock_dao_delete_user)
+    cs = CustomerService()
+
+    user1 = User('user1', 1001)
+    user1.set_num_active_accounts(2)
+
+    actual = cs.delete_user(user1)
+
+    assert actual == f"All customer and account data for user1 " \
+                     f"has been deleted."
+
+
+def test_dao_delete_user_negative(mocker):
+
+    def mock_dao_get_all_users(self):
+        user1 = User('user1', '1001')
+        user1.set_num_active_accounts(2)
+        user2 = User('user2', '1002')
+        user2.set_num_active_accounts(1)
+        users = [user1, user2]
+        return users
+
+    def mock_dao_get_user(self, user_id):
+        if user_id == 1001:
+            user1 = User('user1', 1001)
+            user1.set_num_active_accounts(2)
+            return user1
+        else:
+            return None
+
+    def mock_dao_delete_user(self, user_object):
+        f"User account for {user_object.get_username()} has been deleted."
+
+    mocker.patch('dao.user_dao.UserDao.get_all_users', mock_dao_get_all_users)
+    mocker.patch('dao.user_dao.UserDao.get_user', mock_dao_get_user)
+    mocker.patch('dao.user_dao.UserDao.delete_user', mock_dao_delete_user)
+    cs = CustomerService()
+
+    user1 = User('user2', 1001)
+    user1.set_num_active_accounts(2)
+
+    with pytest.raises(InvalidParamError) as excinfo:
+        actual = cs.delete_user(user1)
+
+    assert str(excinfo.value) == "Cannot confirm user information."
